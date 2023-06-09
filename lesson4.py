ae9b5e0921e66048f208723d6c14abc5bcedee69
Verenich Tatiana <verenik91@gmail.com>
2023-06-10 00:23:08 +0300

    добавлена лекция 4

--- lesson4.py
+++ lesson4.py
@@ -3,15 +3,15 @@
 
 # calc1 = lambda a, b: a + b
 
-"""
-def calc2(a, b):
-    return a * b
 
-def math(op, x, y):
-    print(op (x, y))
+# def calc2(a, b):
+#     return a * b
 
-math(lambda a, b: a + b, 5, 45)
-"""
+# def math(op, x, y):
+#     print(op (x, y))
+
+# math(lambda a, b: a + b, 5, 45)
+
 
 # 1. В списке хранятся числа. Нужно выбрать только чётные числа и составить
 # список пар (число; квадрат числа).
@@ -51,7 +51,79 @@ print(list_1)
 
 # Задача: C клавиатуры вводится некий набор чисел, в качестве разделителя используется
 # пробел. Этот набор чисел будет считан в качестве строки. Как превратить list строк в list чисел?
-
-data = '1 2 3 5 8 15 23 38'.split()
+"""
+data = '1 2 3 5 8 15 23 38'
 print(data)
-data = list(map(int,input().split()))
+data = list(map(int,data.split()))
+print(data)
+"""
+
+# # filter
+# data = [15, 65, 9, 36, 175]
+# res = list(filter(lambda x: x % 10 == 5, data))
+# print(data)
+# print(res)
+
+# ИТОГ:
+# map = # def select(f, col):
+#       #    return [f(x) for x in col]
+  
+# filter = # def where(f, col):
+#          #     return [x for x in col if f(x)]
+# Наш изначальный ког получится таким:
+"""
+data = [1, 2, 3, 5, 8, 15, 23, 38]
+res = map(int, data)
+res = filter(lambda x: x % 2 == 0, res)
+res = list(map(lambda x: (x, x**2), res))
+print(res)
+"""
+
+# #zip
+# #пример1
+# users = ['user1', 'user2', 'user3', 'user4']
+# ids = [4, 5, 6, 7]
+# data = list(zip(users, ids))
+# print(data)
+
+# #пример2
+# users = ['us1', 'us2', 'us3', 'us4', 'us5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data)
+
+# # enumerate
+# users = ['user1', 'user2', 'user3']
+# data = list(enumerate(users))
+# print(data)
+
+# работа с файлами
+"""
+1. Режим a
+"""
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a') # здесь указываем режим, в котором будем работать
+# data.writelines(colors) # разделителей не будет
+# data.close()
+###############
+# with open('file.txt', 'w') as data:
+#     data.write('line 1\n')
+#     data.write('line 2\n')
+"""
+2. Режим r
+"""
+# # ● Чтение данных из файла:
+# path = 'file.txt'
+# data = open('file.txt', 'r')
+# for line in data:
+#     print(line)
+# data.close()
+"""
+3. Режим w
+"""
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'w')
+# data.writelines(colors) # разделителей не будет
+# data.close()
+
--- modul.py
+++ modul.py
@@ -21,7 +21,7 @@
 # n = int(input("Введите номер числа Фибоначчи: "))
 # print(fib(n + 1))
 
-
+#быстрая сортировка
 def quicksort(array):
     if len(array) < 2:
         return array
@@ -30,6 +30,35 @@ def quicksort(array):
     less = [i for i in array[1:] if i <= pivot]
     greater = [i for i in array[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
-print(quicksort([10, 5, 2, 3]))
+print(quicksort([38, 27, 43, 3, 9, 82, 10]))
 
-if 
+
+#сортировка слиянием
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+nums = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(nums)
+print(nums)
